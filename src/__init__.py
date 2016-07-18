e2dc3bc02b2274b42fefc2e640145a54fd435d56
Priyadarshini Ravi <priyadharshiniravi03@gmail.com>
2016-07-18 22:58:53 +0530

    Priya: made some fix

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,6 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(__name__)
 application.config['DEBUG'] = True
-application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://saranganesan:whatever@tw-flask-demo.cv1vih0jkyyw.us-west-2.rds.amazonaws.com/Home_Service'
-application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-db = SQLAlchemy(application)
 
 import src.services.pricing_engine_service
