a83f5d404b05b2aa728e91c0c4457f59f91585c4
Priyadarshini Ravi <priyadharshiniravi03@gmail.com>
2016-07-18 17:47:05 +0530

    Nimesh/Priya: Check Post for proper data

--- src/services/pricing_engine_service.py
+++ src/services/pricing_engine_service.py
@@ -1,4 +1,5 @@
 import flask
+from flask import request
 
 from src import application
 
@@ -10,6 +11,12 @@ def hello():
 
 @application.route('/host-pricing-engine/compute-price', methods=['POST'])
 def calculate_price():
+    host_split = request.form['host_split']
+    guest_price = request.form['guest_price']
+    channel_fees = request.form['channel_fees']
+    vat_percentage = request.form['vat_percentage']
+    floor_price = request.form['floor_price']
+
     return flask.jsonify(host_share=50.0,
                          ofs_share=5000.0,
                          vat=90.0
--- tests/serviceTests/pricing_engine_service_test.py
+++ tests/serviceTests/pricing_engine_service_test.py
@@ -41,5 +41,21 @@ class PricingEngineServiceTest(TestCase):
         self.assertIsNotNone(json_data['vat'])
         self.assertIsNotNone(json_data['ofs_share'])
 
+    def test_not_calculate_price_without_all_data(self):
+        d = dict()
+
+        result = self.application.post('/host-pricing-engine/compute-price', data=d)
+
+        self.assertEqual(result.status_code, 400)
+
+    def test_not_calculate_price_with_partial_data(self):
+        d = dict(host_split=50.0,
+                 guest_price=4500.0,
+                 channel_fees=10.5)
+
+        result = self.application.post('/host-pricing-engine/compute-price', data=d)
+
+        self.assertEqual(result.status_code, 400)
+
 if __name__ == '__main__':
     unittest.main()
